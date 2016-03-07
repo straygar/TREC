ef1abf675d8d78b7ac7f0a0ce5cf9beceb3cc829
unknown <2127555k@BO706-36.ad.dcs.gla.ac.uk>
2016-03-07 13:20:33 +0000

    registration

---
+++ trec/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+#class MyRegistrationView(RegistrationView):
+    #def get_success_url(self,request, user):
+        #return '/rango/'
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'trec.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^accounts/register/$',MyRegistrationView.as_view(),name='registration_register'),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+)
