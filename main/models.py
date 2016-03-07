03edab5c3e1b240c0d512aee27764d47b562082e
Pablo Arteaga <pablo@pabloarteaga.es>
2016-03-07 13:57:59 +0000

    Added user model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Researcher(models.Model):
+    profile_picture = models.CharField(max_length=64, unique=True)
+    website = models.CharField(max_length=64, unique=False)
+    display_name = models.CharField(max_length=128, unique=False)
+    organization = models.CharField(max_length=128, unique=False)
+
+
--- trec/settings.py
+++ trec/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_PATH="/templates/"
+TEMPLATE_PATH=os.path.join(BASE_DIR, "templates")
+
+TEMPLATE_DIRS = (TEMPLATE_PATH,)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
