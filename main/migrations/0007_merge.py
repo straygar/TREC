87f8e16cc731914212952a96fde243a172cde1e3
Shaun Macdonald <sevendaysinthesun@gmail.com>
2016-03-15 20:13:05 +0000

    homepage updates

---
+++ main/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0006_run_result_file'),
+        ('main', '0003_auto_20160313_1202'),
+    ]
+
+    operations = [
+    ]
--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,11 @@ import views
 
 urlpatterns = patterns('',
     url(r'^profile/$',views.profile,name='profile'),
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.home,name='home'),
     url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^edit_profile/$',views.edit_profile,name='edit_profile'),
     url(r'^about/$',views.about,name='about'),
+    url(r'^uploadRun/$', views.uploadRun, name='uploadRun'),
     )
