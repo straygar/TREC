0c0a1865743ef6af4146be1b9613ae3d817615bf
Aigars <nastyguard@gmail.com>
2016-03-18 20:30:41 +0200

    Added extra material design elements, footer, adjusted some layout design consistency.

--- main/forms.py
+++ main/forms.py
@@ -26,13 +26,14 @@ class RunFileForm(forms.ModelForm):
         fields = ('result_file',)
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(help_text="Please enter a username.")
-    email = forms.CharField(help_text="Please enter your email.")
-    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
+    username = forms.CharField(help_text="Username")
+    email = forms.CharField(help_text="E-mail")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Password")
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+		
 
 class UserProfileForm(forms.ModelForm):
 
