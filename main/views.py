89f353226ae364e4e34ea411d9e161691795c3c0
Shaun Macdonald <sevendaysinthesun@gmail.com>
2016-03-19 17:38:48 +0000

    added Get My Runs button to browse

--- main/views.py
+++ main/views.py
@@ -33,8 +33,13 @@ def index(request):
 def browse(request):
     contextDict = {}
     accepted = False
+    userRunsRequested = False
     if request.method == "GET":
         browse_form = BrowseForm()
+        if request.GET.get('userRuns'):
+            userRunsRequested = True
+            user_runs = Run.objects.filter(researcher__user=request.user)
+            contextDict["userRuns"] = user_runs
     else:
         browse_form = BrowseForm(request.POST)
         if browse_form.is_valid():
@@ -45,10 +50,15 @@ def browse(request):
             run_list = Run.objects.filter(task = browse_form.cleaned_data["task"])
             contextDict["runs"] = run_list
 
+
+    contextDict["user"] = request.user
     contextDict["form"] = browse_form
     contextDict["accepted"] = accepted
+    contextDict["userRunsRequested"] = userRunsRequested
+
 
     return render(request, 'main/browse.html', contextDict)
+
 
 @login_required
 def uploadRun(request):
