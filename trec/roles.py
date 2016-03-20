fa1305134752ec4cda478a5b463d19c611e5cc39
Pablo Arteaga <Pablo Arteaga>
2016-03-20 03:33:07 +0000

    New roles

--- trec/roles.py
+++ trec/roles.py
@@ -3,13 +3,10 @@ from rolepermissions.roles import AbstractUserRole
 class Researcher(AbstractUserRole):
     available_permissions = {
         'upload_file': True,
-        'send_message': True,
     }
 
 class Admin(AbstractUserRole):
     available_permissions = {
         'upload_file': True,
-        'send_message': True,
         'edit_tracks': True,
-        'bypass_captcha': True,
     }
